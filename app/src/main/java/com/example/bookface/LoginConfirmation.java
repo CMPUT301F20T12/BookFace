15b566ea30812ad3a8157caaa23d202292e0baab
poulomi-g <poulomi@ualberta.ca>
2020-10-26 03:31:17 +0530

    Added signout option

--- app/src/main/java/com/example/bookface/LoginConfirmation.java
+++ app/src/main/java/com/example/bookface/LoginConfirmation.java
@@ -2,13 +2,37 @@ package com.example.bookface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginConfirmation extends AppCompatActivity {
+
+    Button buttonSignout;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_confirmation);
+
+        buttonSignout = findViewById(R.id.signOut);
+
+        buttonSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+
+                Intent i = new Intent(LoginConfirmation.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
