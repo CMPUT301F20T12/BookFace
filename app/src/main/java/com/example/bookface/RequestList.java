106de529ca9b01e5cc7a4f5911bb1f17fad4ad1a
Michelle Lee <mlee1@ualberta.ca>
2020-10-25 11:00:31 -0600

    Request Class Added

---
+++ app/src/main/java/com/example/bookface/RequestList.java
@@ -0,0 +1,16 @@
+package com.example.bookface;
+
+import java.util.*;
+
+
+public class RequestList {
+    private List<Book> requests = new ArrayList<>();
+
+    public void acceptRequest(Request request) {
+        request.setRequestStatus("accepted");
+    }
+
+    public void declineRequest(Request request) {
+        request.setRequestStatus("declined");
+    }
+}
