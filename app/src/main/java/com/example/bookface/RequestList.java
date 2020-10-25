40a8ee71d69bcf54cef320067f7a6b47cd88564e
Michelle Lee <mlee1@ualberta.ca>
2020-10-25 11:14:17 -0600

    RequestList Class Added

--- app/src/main/java/com/example/bookface/RequestList.java
+++ app/src/main/java/com/example/bookface/RequestList.java
@@ -4,14 +4,24 @@ package com.example.bookface;
 
 import java.util.*;
 
-
+/**
+ * This is a class that keeps track of a list of requests
+ */
 public class RequestList {
     private List<Book> requests = new ArrayList<>();
 
+    /**
+     * This changes the status of the request as accepted
+     * @param request
+     */
     public void acceptRequest(Request request) {
         request.setRequestStatus("accepted");
     }
 
+    /**
+     * This changes the status of the request as declined
+     * @param request
+     */
     public void declineRequest(Request request) {
         request.setRequestStatus("declined");
     }
