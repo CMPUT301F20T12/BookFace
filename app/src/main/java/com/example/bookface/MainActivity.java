4d0376a241714ae4b3eae53e23e2a67d4fd6f088
Ashwin Mahesh <ashwin@quze.co>
2020-10-25 21:36:23 -0600

    activity_add_edit_book added

--- app/src/main/java/com/example/bookface/MainActivity.java
+++ app/src/main/java/com/example/bookface/MainActivity.java
@@ -11,8 +11,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_edit_book);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
     }
 }
