ba176ed3e63bb93e46aa7708b36c5216b92c05d8
Ashwin Mahesh <ashwin@quze.co>
2020-11-04 19:29:54 -0700

    Add Book Scanning functionality Added

---
+++ app/src/main/java/com/example/bookface/AddEditBookActivity.java
@@ -0,0 +1,122 @@
+package com.example.bookface;
+
+import android.content.Intent;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.toolbox.Volley;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+
+public class AddEditBookActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView scan;
+    private EditText isbn;
+    private EditText author;
+    private EditText title;
+    private EditText description;
+
+    private RequestQueue mRequestQueue;
+    private static  final  String BASE_URL="https://www.googleapis.com/books/v1/volumes?q=";
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit_book);
+
+        scan = findViewById(R.id.scanBookButton);
+        isbn = findViewById(R.id.editISBN);
+        author = findViewById(R.id.editName);
+        title = findViewById(R.id.editTitle);
+        description = findViewById(R.id.editDescription);
+        mRequestQueue = Volley.newRequestQueue(this);
+        scan.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Scan scanObj = new Scan(AddEditBookActivity.this);
+        scanObj.scanCode();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            String code = result.getContents();
+            if (code != null) {
+                this.extractContents(code);
+            }
+        }
+    }
+
+    public void parseJson(String key, String code) {
+        final JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, key.toString(), null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray items = response.getJSONArray("items");
+                            for (int i = 0; i < items.length(); i++) {
+                                JSONObject item = items.getJSONObject(i);
+                                JSONObject volumeInfo = item.getJSONObject("volumeInfo");
+                                JSONArray identifiers = volumeInfo.getJSONArray("industryIdentifiers");
+                                int flag = 0;
+                                for (int j = 0; j < identifiers.length(); j++) {
+                                    try {
+                                        String identifier = identifiers.getJSONObject(j).getString("identifier");
+                                        if (code.equals(identifier)) {
+                                            flag = 1;
+                                            break;
+                                        }
+                                    } catch (Exception e) {
+                                    }
+                                }
+                                if (flag == 1) {
+                                    isbn.setText(code);
+                                    title.setText(volumeInfo.getString("title"));
+                                    description.setText(volumeInfo.getString("description"));
+                                    JSONArray authors = volumeInfo.getJSONArray("authors");
+                                    author.setText(authors.join(", "));
+                                    break;
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.e("TAG" , e.toString());
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mRequestQueue.add(request);
+    }
+
+    private void extractContents(String code) {
+        Uri uri=Uri.parse(BASE_URL+code);
+        Uri.Builder buider = uri.buildUpon();
+        this.parseJson(buider.toString(), code);
+    }
+
+}
--- app/src/main/java/com/example/bookface/MainActivity.java
+++ app/src/main/java/com/example/bookface/MainActivity.java
@@ -1,22 +1,14 @@
 package com.example.bookface;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,91 +24,94 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Intent addEdit = new Intent(MainActivity.this, AddEditBookActivity.class);
+        startActivity(addEdit);
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        emailID = findViewById(R.id.editTextTextEmailAddress);
-        password = findViewById(R.id.editTextNumberPassword);
-        buttonLogin = findViewById(R.id.button);
-        SignupPrompt = findViewById(R.id.textView);
-
-        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-
-                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
-                if (mFirebaseUser != null){
-                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                    Intent i = new Intent(MainActivity.this, LoginConfirmationActivity.class);
-                    startActivity(i);
-                }
-                else{
-                    Toast.makeText(MainActivity.this, "Not Logged In", Toast.LENGTH_SHORT).show();
-                }
-            }
-        };
-
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String email = emailID.getText().toString();
-                String pwd = password.getText().toString();
-
-
-                if (email.isEmpty() || pwd.isEmpty()){
-
-                    if(email.isEmpty()){
-                        emailID.setError("Enter email");
-                        emailID.requestFocus();
-                    }
-                    if (pwd.isEmpty()){
-                        password.setError("Enter password");
-                        password.requestFocus();
-                    }
-
-                    if (email.isEmpty() && pwd.isEmpty()){
-                        Toast.makeText(MainActivity.this, "Both fields empty", Toast.LENGTH_SHORT).show();
-                    }
-
-                }
-
-                // When email and password are both valid
-                else if (!(email.isEmpty() && pwd.isEmpty())){
-                    mFirebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(!task.isSuccessful()){
-                                Toast.makeText(MainActivity.this, "Login Unsuccessful", Toast.LENGTH_SHORT).show();
-                            }
-                            else{
-                                Intent toLoginConfirmation = new Intent(MainActivity.this, LoginConfirmationActivity.class);
-                                startActivity(toLoginConfirmation);
-                            }
-                        }
-                    });
-                }
-
-                else {
-                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
-
-        SignupPrompt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent toSignup = new Intent(MainActivity.this, SignupActivity.class);
-                startActivity(toSignup);
-            }
-        });
-
+//        FirebaseFirestore db = FirebaseFirestore.getInstance();
+//
+//        mFirebaseAuth = FirebaseAuth.getInstance();
+//        emailID = findViewById(R.id.editTextTextEmailAddress);
+//        password = findViewById(R.id.editTextNumberPassword);
+//        buttonLogin = findViewById(R.id.button);
+//        SignupPrompt = findViewById(R.id.textView);
+//
+//        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+//            @Override
+//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+//
+//                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+//                if (mFirebaseUser != null){
+//                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+//                    Intent i = new Intent(MainActivity.this, LoginConfirmationActivity.class);
+//                    startActivity(i);
+//                }
+//                else{
+//                    Toast.makeText(MainActivity.this, "Not Logged In", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        };
+//
+//        buttonLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                String email = emailID.getText().toString();
+//                String pwd = password.getText().toString();
+//
+//
+//                if (email.isEmpty() || pwd.isEmpty()){
+//
+//                    if(email.isEmpty()){
+//                        emailID.setError("Enter email");
+//                        emailID.requestFocus();
+//                    }
+//                    if (pwd.isEmpty()){
+//                        password.setError("Enter password");
+//                        password.requestFocus();
+//                    }
+//
+//                    if (email.isEmpty() && pwd.isEmpty()){
+//                        Toast.makeText(MainActivity.this, "Both fields empty", Toast.LENGTH_SHORT).show();
+//                    }
+//
+//                }
+//
+//                // When email and password are both valid
+//                else if (!(email.isEmpty() && pwd.isEmpty())){
+//                    mFirebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<AuthResult> task) {
+//                            if(!task.isSuccessful()){
+//                                Toast.makeText(MainActivity.this, "Login Unsuccessful", Toast.LENGTH_SHORT).show();
+//                            }
+//                            else{
+//                                Intent toLoginConfirmation = new Intent(MainActivity.this, LoginConfirmationActivity.class);
+//                                startActivity(toLoginConfirmation);
+//                            }
+//                        }
+//                    });
+//                }
+//
+//                else {
+//                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+//                }
+//
+//            }
+//        });
+//
+//        SignupPrompt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent toSignup = new Intent(MainActivity.this, SignupActivity.class);
+//                startActivity(toSignup);
+//            }
+//        });
+//
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
-    }
+//
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+//    }
 }
