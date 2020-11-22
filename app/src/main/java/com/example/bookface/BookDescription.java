c10ce6fe7f78d8c4b65346abac0e1f480378bd74
Michelle Lee <mlee1@ualberta.ca>
2020-11-21 23:26:55 -0700

    Book Description from Item Click

--- app/src/main/java/com/example/bookface/BookDescription.java
+++ app/src/main/java/com/example/bookface/BookDescription.java
@@ -5,17 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
-import android.view.MotionEvent;
+// import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ScrollView;
+// import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
+// import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+// import org.w3c.dom.Text;
 
 public class BookDescription extends AppCompatActivity {
+
+    FirebaseAuth mFirebaseAuth;
+    FirebaseUser userInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +29,13 @@ public class BookDescription extends AppCompatActivity {
         setContentView(R.layout.activity_book_description);
 
         // Retrieve the objects passed into the intent object
+        Bundle data = getIntent().getExtras();
+        Book book = data.getParcelable("book");
+
         // TODO
         // Declare the fireAuth variable to get the currentUser()
+//        userInstance = mFirebaseAuth.getCurrentUser();
+//        String currentUserName = userInstance.getDisplayName();
 
         Button btnEdit = findViewById(R.id.editButton);
         Button btnTop = findViewById(R.id.myBooksOrSearchButton);
@@ -40,64 +51,66 @@ public class BookDescription extends AppCompatActivity {
         ImageView image = (ImageView) findViewById(R.id.imageView);
 
         // TODO
-//        if (book.owner != currentUser) {
-//            btnTop.setText("Search");
-//            btnEdit.setVisibility(View.INVISIBLE);
-//            btnBottom.setText("Send Request");
-//            btnTop.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    // Call Search Books Activity
-//                }
-//            });
-//
-//            btnBottom.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    // Call Send Request Activity
-//                }
-//            });
-//            textBorrower.setVisibility(View.INVISIBLE);
-//        }
-//        else {
-            btnEdit.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    // Call Add/Edit Book activity
-                }
-            });
+////        if (book.getOwnerUsername() != currentUserName) { // If the user is not the owner  (called from search page)
+            btnTop.setText("Search");
+            btnEdit.setVisibility(View.INVISIBLE);
+            btnBottom.setText("Send Request");
 
             btnTop.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    // Call MyBooks Activity
+                    // Call Search Books Activity
+                    Intent intent = new Intent(BookDescription.this, SearchActivity.class);
+                    startActivity(intent);
                 }
             });
 
             btnBottom.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    // Call the functionality to collect the book
+                    // Call Send Request Activity
+
                 }
             });
+            textBorrower.setVisibility(View.INVISIBLE);
+//        }
+//        else {
+//            btnEdit.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    // Call Add/Edit Book activity
+//                }
+//            });
+//
+//            btnTop.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    // Call MyBooks Activity
+//                }
+//            });
+//
+//            btnBottom.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    // Call the functionality to collect the book
+//                }
+//            });
 //            textBorrower.setText(book.getBorrowerUsername().toString());
 //        }
 
+        textAuthor.setText(book.getAuthor());
+        textDescHeading.setText(Html.fromHtml("<b>DESCRIPTION:</b>"));
+        textIsbn.setText(book.getISBN());
+        textDescription.setText(book.getDescription());
+        textStatus.setText(book.getStatus());
+        textTitle.setText(Html.fromHtml("<b>" + book.getTitle().toString()+"</b>"));
+
         // TODO
-//        textAuthor.setText(book.getAuthor().toString());
-//        textDescHeading.setText(Html.fromHtml("<b>DESCRIPTION:</b>"));
-//        textIsbn.setText(book.getIsbn().toString());
-//        textDescription.setText(book.getDescription().toString());
-//        textStatus.setText(book.getStatus.toString());
-//        textTitle.setText(Html.fromHtml("<b>" + book.getTitle().toString()+"</b>"));
-
-
 //        Use android:background="@drawable/<ImageFileName>" in the relative layout in xml file.
 //        Only catch is that I need to put the image file in drawable folder in app/res/drawable in order to display it
 
 
     }
-
 
 //    public void clickImage(View view) {
 //        Intent showImage = new Intent(BookDescription.this, AddImage.class);
