145e9f6e615425d41ee0a084e633f251ecaa70f6
poulomi-g <poulomi@ualberta.ca>
2020-11-30 08:05:01 +0530

    Checking token func

--- app/src/main/java/com/example/bookface/Notifications/MyFirebaseInstanceId.java
+++ app/src/main/java/com/example/bookface/Notifications/MyFirebaseInstanceId.java
@@ -1,4 +1,49 @@
 package com.example.bookface.Notifications;
 
-public class MyFirebaseInstanceId {
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyFirebaseInstanceId extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        System.out.print(firebaseUser.toString());
+
+        String refreshToken = FirebaseInstanceId.getInstance().getToken();
+
+        if (firebaseUser != null){
+            updateToken(refreshToken);
+        }
+
+    }
+
+    private void updateToken(String refreshToken){
+        Log.d("tokenchecker", "Token getting updated");
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        String username = firebaseUser.getDisplayName();
+        System.out.println(username);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Token token = new Token(refreshToken);
+
+
+    }
 }
